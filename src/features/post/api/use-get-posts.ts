796d576d6bc636394e2a44f9da3c58db2e4c3f72
samurun <fadlan.8291@gmail.com>
2024-10-28 10:12:41 +0700

    refactor(posts): update query key and remove unused code

--- src/features/post/api/use-get-posts.ts
+++ src/features/post/api/use-get-posts.ts
@@ -1,9 +1,9 @@
-import { getPostById, getPosts } from '@/features/post/actions';
+import { getPosts } from '@/features/post/actions';
 import { useQuery } from '@tanstack/react-query';
 
 export function useGetPosts() {
   const query = useQuery({
-    queryKey: ['post'],
+    queryKey: ['posts'],
     queryFn: async () => {
       const res = await getPosts();
       return res;
