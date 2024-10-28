bbd2cf23dd7d7dda10face12261a80abaa05c5cc
samurun <fadlan.8291@gmail.com>
2024-10-28 10:04:59 +0700

    chore: install js-cookie and sonner libraries

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,15 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
 import { QueryProvider } from './qury-provider';
+import { Toaster } from './ui/sonner';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryProvider>
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        {children}
+        <Toaster richColors />
+      </SessionProvider>
     </QueryProvider>
   );
 }
